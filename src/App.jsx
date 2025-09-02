3eb6195678b76f54d4d09117dfdd6f2e669072ce
sadia-mushtaq50 <sadiamushtaq.official.com@gmail.com>
2025-09-02 21:04:39 +0500

    Initial project upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import Home from './components/Home'
 
-import Tshirtdesign from '../public/Tshirtdesign'
+import Tshirtdesign from './components/Tshirtdesign'
 function App() {
   
 
